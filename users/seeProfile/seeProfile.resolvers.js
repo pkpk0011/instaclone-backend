3fbdded1c0110980b57ac1c3ef40990dce25ecf4
Heesang <ekgns1354@gmail.com>
2021-06-23 15:29:50 +0900

    start photos

---
+++ users/seeProfile/seeProfile.resolvers.js
@@ -0,0 +1,15 @@
+import client from "../../client";
+
+export default {
+    Query: {
+        seeProfile: (_, { userName }) => client.user.findUnique({
+            where: {
+                userName,
+            },
+            include: {
+                following: true,
+                followers: true,
+            }
+        }),
+    },
+};
